3386e1c3c077aebdb6195616446f8a0ec9a10f8a
Ishwar Pawar <97723377+IshwarBPawar@users.noreply.github.com>
2026-02-16 16:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Jenkins + Docker this edit to check automation \n');
+  res.end('Hello from Jenkins + Docker this edit to check automation this changes happens through github \n');
 });
 
 server.listen(PORT, () => {
